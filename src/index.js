cbc28e4f97a7ffc01e349f198a910e50933cf8da
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-16 18:06:33 -0300

    saving data to local array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -13,34 +14,22 @@ const app = express();
  * 
  */
 
+ /**
+  * Tipos de Parâmetros:
+  * 
+  * Query Params: Filtros e Paginação
+  * Route Params: Identificar Recursos (PUT/DELETE)
+  * Request Body: Conteúdo para POST ou PUT (JSON)
+  */
+
 app.use(express.json())
 
+const projetos = []
+
 app.get('/projetos', (request, response) => {
-  return response.json([{
-      id: "5df3eecd4f2c0a006b466f32",
-      name: "alpha",
-      status: "active",
-      owner: "5da5d38795ffb700729fc0e4",
-      createdAt: "2020-11-06T15:40:00.000Z",
-      updatedAt: "2020-11-06T15:40:00.000Z",
-    },
-    {
-      id: "5df3eecd4f2c0a006b466f32",
-      name: "vldmrt",
-      status: "done",
-      owner: "5da5d38795ffb700729fc0e4",
-      createdAt: "2020-11-06T15:40:00.000Z",
-      updatedAt: "2020-11-06T15:40:00.000Z",
-    },
-    {
-      id: "5df3eecd4f2c0a006b466f32",
-      name: "gama",
-      status: "future",
-      owner: "5da5d38795ffb700729fc0e4",
-      createdAt: "2020-11-06T15:40:00.000Z",
-      updatedAt: "2020-11-06T15:40:00.000Z",
-    },
-  ])
+  const { owner } = request.query;
+  const resultados = owner ? projetos.filter( projeto => projeto.owner.includes(owner) ) : projetos;
+  return response.json(resultados);
 });
 
 app.post('/projetos', (request, response) => {
@@ -51,39 +40,61 @@ app.post('/projetos', (request, response) => {
     createdAt,
     updatedAt
   } = request.body;
-  const id = "5df3eecd4f2c0a006b466f32";
-  return response.json({
+  const projeto = {
+    id: uuid(),
+    name,
+    status,
+    owner,
+    createdAt,
+    updatedAt
+  }
+  projetos.push(projeto);
+  return response.json(projeto);
+})
+
+app.put('/projetos/:id', (request, response) => {
+  const { 
+    name,
+    status,
+    owner,
+    createdAt,
+    updatedAt
+   } = request.body;
+  
+   const id = request.params.id;
+  
+  const indexProjeto = projetos.findIndex( projeto => projeto.id === id)
+  
+  if (indexProjeto < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado' })
+  }
+
+  const projeto = {
     id,
     name,
     status,
     owner,
     createdAt,
     updatedAt
-  })
-})
+  }
 
-app.put('/projetos/:id', (request, response) => {
-  const id = request.params.id;
-  return response.json({
-    id,
-    name: "Delta",
-    status: "active",
-    owner: "5da5d38795ffb700729fc0e4",
-    createdAt: "2020-11-06T15:40:00.000Z",
-    updatedAt: "2020-11-06T15:40:00.000Z"
-  })
+  projetos[indexProjeto] = projeto;
+
+  return response.json(projeto)
 })
 
 app.delete('/projetos/:id', (request, response) => {
   const id = request.params.id;
-  return response.json({
-    id,
-    name: "Delta",
-    status: "deleted",
-    owner: "5da5d38795ffb700729fc0e4",
-    createdAt: "2020-11-06T15:40:00.000Z",
-    updatedAt: "2020-11-06T15:40:00.000Z"
-  })
+
+  const indexProjeto = projetos.findIndex( projeto => projeto.id === id)
+  
+  if (indexProjeto < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado' })
+  }
+
+  projetos.splice(indexProjeto, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(3333, () => {
